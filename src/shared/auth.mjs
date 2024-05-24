7b53fe5c7ed6386bc1aba5ca3c60a9105a21503e
Rafał Więcek <gitzzz14@gmail.com>
2024-05-24 16:03:51 +0200

    Biome

--- src/shared/auth.mjs
+++ src/shared/auth.mjs
@@ -1,22 +1,27 @@
-import { google } from "googleapis";
-import { existsSync, readFileSync } from "fs";
+import { existsSync, readFileSync } from 'node:fs';
+import { google } from 'googleapis';
 
 export async function getAccessToken() {
-  if (!existsSync("./service_account.json")) {
-    console.error("❌ service_account.json not found, please follow the instructions in README.md");
-    console.error("");
-    process.exit(1);
-  }
+    if (!existsSync('./service_account.json')) {
+        console.error(
+            '❌ service_account.json not found, please follow the instructions in README.md',
+        );
+        console.error('');
+        process.exit(1);
+    }
 
-  const key = JSON.parse(readFileSync("./service_account.json", "utf8"));
-  const jwtClient = new google.auth.JWT(
-    key.client_email,
-    null,
-    key.private_key,
-    ["https://www.googleapis.com/auth/webmasters.readonly", "https://www.googleapis.com/auth/indexing"],
-    null
-  );
+    const key = JSON.parse(readFileSync('./service_account.json', 'utf8'));
+    const jwtClient = new google.auth.JWT(
+        key.client_email,
+        null,
+        key.private_key,
+        [
+            'https://www.googleapis.com/auth/webmasters.readonly',
+            'https://www.googleapis.com/auth/indexing',
+        ],
+        null,
+    );
 
-  const tokens = await jwtClient.authorize();
-  return tokens.access_token;
+    const tokens = await jwtClient.authorize();
+    return tokens.access_token;
 }
